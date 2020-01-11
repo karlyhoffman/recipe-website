649fb5004de0b5a26fc05177b6c049a112e09e0f
Karly <karlyhoffman@gmail.com>
2020-01-11 10:59:23 -0600

    tags page — initial build

---
+++ pages/tags/index.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import getCookies from 'next-cookies';
+import { fetchDocumentsByType } from '../../utils/prismic';
+import styles from '../../styles/homepage.scss';
+
+class TagsOverview extends Component {
+  static async getInitialProps(context) {
+    const { req, res } = context;
+    const nextCookies = getCookies(context);
+    const ref = nextCookies['io.prismic.preview'] || null;
+
+    const [
+      cuisineTags,
+      ingredientTags,
+      timeTags,
+      seasonTags,
+      dishTypeTags,
+      dayTags
+    ] = await Promise.all([
+      fetchDocumentsByType({ type: 'cuisine_tag', req }),
+      fetchDocumentsByType({ type: 'ingredient_tag', req }),
+      fetchDocumentsByType({ type: 'time_tag', req }),
+      fetchDocumentsByType({ type: 'season_tag', req }),
+      fetchDocumentsByType({ type: 'type_tag', req }),
+      fetchDocumentsByType({ type: 'day_tag', req })
+    ]);
+
+    if (res)
+      res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate');
+
+    return {
+      cuisineTags: cuisineTags.results || [],
+      ingredientTags: ingredientTags.results || [],
+      timeTags: timeTags.results || [],
+      seasonTags: seasonTags.results || [],
+      dishTypeTags: dishTypeTags.results || [],
+      dayTags: dayTags.results || []
+    };
+  }
+
+  fetchRecipeByTagType = () => {
+    // TODO: listen for tag click, update results and query params
+  };
+
+  render() {
+    const {
+      cuisineTags,
+      ingredientTags,
+      timeTags,
+      seasonTags,
+      dishTypeTags,
+      dayTags
+    } = this.props;
+
+    console.log({
+      cuisineTags,
+      ingredientTags,
+      timeTags,
+      seasonTags,
+      dishTypeTags,
+      dayTags
+    });
+
+    return (
+      <div id="tags-overview">
+        <h1>View Recipes By Tags</h1>
+
+        <style jsx>{styles}</style>
+      </div>
+    );
+  }
+}
+
+export default TagsOverview;
