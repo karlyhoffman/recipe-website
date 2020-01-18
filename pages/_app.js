c6a08808cb25688a94d84e4005dd31b77d2a893f
Karly <karlyhoffman@gmail.com>
2020-01-17 20:05:54 -0600

    head — add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import Head from 'next/head';
 import App from 'next/app';
@@ -11,7 +12,14 @@ export default class RecipeApp extends App {
     return (
       <>
         <Head>
-          <title>Reach Dev Showcase</title>
+          <title>5047 Cooking</title>
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#cc8b0e" />
+          <meta name="msapplication-TileColor" content="#00aba9" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <Navbar />
         <Component {...pageProps} />
