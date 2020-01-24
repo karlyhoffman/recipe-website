b155a05763cd12db279c9fd4b3b024751e35b133
Karly <karlyhoffman@gmail.com>
2020-01-23 20:55:21 -0600

    tags overview page — render tag category links

--- pages/tags/index.js
+++ pages/tags/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import getCookies from 'next-cookies';
-import { fetchDocumentsByType } from '../../utils/prismic';
-import '../../styles/pages/homepage.scss';
+import Link from 'next/link';
+import '../../styles/pages/tags-overview.scss';
 
 class TagsOverview extends Component {
   static async getInitialProps(context) {
@@ -9,56 +9,45 @@ class TagsOverview extends Component {
     const nextCookies = getCookies(context);
     const ref = nextCookies['io.prismic.preview'] || null;
 
-    const [
-      cuisineTags,
-      ingredientTags,
-      seasonTags,
-      dishTypeTags,
-      weekdayTags
-    ] = await Promise.all([
-      fetchDocumentsByType({ type: 'cuisine_tag', req }),
-      fetchDocumentsByType({ type: 'ingredient_tag', req }),
-      fetchDocumentsByType({ type: 'season_tag', req }),
-      fetchDocumentsByType({ type: 'type_tag', req }),
-      fetchDocumentsByType({ type: 'weekday_tag', req })
-    ]);
-
     if (res)
       res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate');
 
-    return {
-      cuisineTags: cuisineTags.results || [],
-      ingredientTags: ingredientTags.results || [],
-      seasonTags: seasonTags.results || [],
-      dishTypeTags: dishTypeTags.results || [],
-      weekdayTags
-    };
+    return {};
   }
 
-  fetchRecipeByTagType = () => {
-    // TODO: listen for tag click, update results and query params
-  };
-
   render() {
-    const {
-      cuisineTags,
-      ingredientTags,
-      seasonTags,
-      dishTypeTags,
-      weekdayTags
-    } = this.props;
-
-    // console.log({
-    //   cuisineTags,
-    //   ingredientTags,
-    //   seasonTags,
-    //   dishTypeTags,
-    //   weekdayTags
-    // });
-
     return (
-      <div id="tags-overview">
-        <h1>View Recipes By Tags</h1>
+      <div id="tags-overview" className="container">
+        <div className="row">
+          <div className="col-12">
+            <h1>Tag Categories</h1>
+          </div>
+          <div className="col-6 col-md-4">
+            <Link href="/tags/ingredients">
+              <a>Ingredients</a>
+            </Link>
+          </div>
+          <div className="col-6 col-md-4">
+            <Link href="/tags/cuisines">
+              <a>Cuisines</a>
+            </Link>
+          </div>
+          <div className="col-6 col-md-4">
+            <Link href="/tags/dish-types">
+              <a>Dish Types</a>
+            </Link>
+          </div>
+          <div className="col-6 col-md-4">
+            <Link href="/tags/seasons">
+              <a>Seasons</a>
+            </Link>
+          </div>
+          <div className="col-6 col-md-4">
+            <Link href="/tags/weekday">
+              <a>Weekday Meals</a>
+            </Link>
+          </div>
+        </div>
       </div>
     );
   }
