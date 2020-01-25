a73147ed137e0a09a5d4626cad328ee342942ce2
Karly <karlyhoffman@gmail.com>
2020-01-25 15:22:17 -0600

    navbar — desktop styling

--- components/navbar.js
+++ components/navbar.js
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import '../styles/components/navbar.scss';
 
 export default () => {
-  // TODO: hookup to CMS
   return (
     <header id="navbar">
       <ul className="container">
@@ -12,10 +11,40 @@ export default () => {
             <a>Home</a>
           </Link>
         </li>
-        <li>
+        <li className="has-sub">
           <Link href="/recipes">
             <a>Recipes</a>
           </Link>
+          <ul className="sub-menu">
+            <li>
+              <span className="label">BY TAG</span>
+            </li>
+            <li>
+              <Link href="/recipes/ingredients">
+                <a>Ingredients</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/recipes/cuisines">
+                <a>Cuisines</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/recipes/dish-types">
+                <a>Dish Types</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/recipes/seasons">
+                <a>Seasons</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/recipes/weekday">
+                <a>Weekday Meals</a>
+              </Link>
+            </li>
+          </ul>
         </li>
         <li>
           <Link href="/groceries">
