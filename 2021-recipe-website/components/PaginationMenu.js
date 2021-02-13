d44af328216a6ed21c4adff74c078b4b5c32b645
Karly <karlyhoffman@gmail.com>
2021-02-13 12:12:56 -0600

    2021 upgrade | PaginationMenu — update link url (add to existing queries instead of replacing)

--- 2021-recipe-website/components/PaginationMenu.js
+++ 2021-recipe-website/components/PaginationMenu.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import styles from 'styles/components/pagination-menu.module.scss';
 
 function PaginationMenu({ totalCount, pageSize, page }) {
-  const { pathname } = useRouter();
+  const { pathname, query } = useRouter();
   const numOfPages = totalCount && pageSize ? Math.ceil(totalCount / pageSize) : 0;
 
   if (numOfPages < 2) return null;
@@ -18,7 +18,10 @@ function PaginationMenu({ totalCount, pageSize, page }) {
             <Link
               href={{
                 pathname,
-                query: { page: index + 1 },
+                query: {
+                  ...query,
+                  page: index + 1
+                },
               }}
             >
               <a>{index + 1}</a>
