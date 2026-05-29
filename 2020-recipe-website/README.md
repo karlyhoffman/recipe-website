be5d379092ccfb7170075c78f27509fb4d711a86
Karly Hoffman <karlyhoffman@gmail.com>
2026-05-29 09:09:16 -0500

    cleanup — remove old nextjs projects
    deletes `2020-recipe-website` and `2021-recipe-website`

--- 2020-recipe-website/README.md
+++
@@ -1,3 +0,0 @@
-# Recipe Website
-
-This is a Next.js project that was originally on version "^9.4.2", before dependabot alerts were updated.
--- 2021-recipe-website/components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Footer } from 'components/Footer';
-export { default as GroceryList } from 'components/GroceryList';
-export { default as Layout } from 'components/Layout';
-export { default as Meta } from 'components/Meta';
-export { default as Navbar } from 'components/Navbar';
-export { default as PaginationMenu } from 'components/PaginationMenu';
-export { default as SearchBar } from 'components/SearchBar';
-export { default as TagOverviewLayout } from 'components/TagOverviewLayout';
-export { default as TagsDetailLayout } from 'components/TagsDetailLayout';
